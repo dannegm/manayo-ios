99fd520cc5d22effa823330e73405280746e983d
dannegm <im@danielgarcia.me>
2025-11-15 18:56:02 -0600

    Initial Commit

---
+++ manayo/manayoApp.swift
@@ -0,0 +1,17 @@
+//
+//  manayoApp.swift
+//  manayo
+//
+//  Created by Daniel García on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct manayoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ manayoTests/manayoTests.swift
@@ -0,0 +1,17 @@
+//
+//  manayoTests.swift
+//  manayoTests
+//
+//  Created by Daniel García on 15/11/25.
+//
+
+import Testing
+@testable import manayo
+
+struct manayoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
